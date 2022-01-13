270503bd7d35f6f350e65210cc3de417b4ea78f6
Henry Cortez <Henry200amaya@gmail.com>
2022-01-13 14:37:48 -0600

    :bug: Fix explication

--- 11/sumNumber/sumNumber.js
+++ 11/sumNumber/sumNumber.js
@@ -1,14 +1,4 @@
-const sumNumbers = (elements = [], sum = { even: 0, odd: 0}) => {
-
-  const last = elements.pop()
-
-  if (!last) return sum
-
-  if (last%2===0) return sumNumbers(elements, { even: sum.even + last, odd: sum.odd })
-
-  return sumNumbers(elements, { even: sum.even, odd: sum.odd + last } )
+const sumNumbers = (elements = []) => {
 }
-
-console.log(sumNumbers([4,1,3,1,5,7,2]))
 
 module.exports = sumNumbers
